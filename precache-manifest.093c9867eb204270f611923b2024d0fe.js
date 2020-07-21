2d36df6bd1c691c8f1ca804780d70fdba6bf432d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-21 20:33:24 +0000

    Deploy RomanovAleksander/movies-rdb to github.com/RomanovAleksander/movies-rdb.git:gh-pages

--- precache-manifest.093c9867eb204270f611923b2024d0fe.js
+++ precache-manifest.093c9867eb204270f611923b2024d0fe.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "779e6b79425d8f43935aa7d9a5d2eda2",
+    "revision": "f7d059ff209feb8b292b8d7e3b76fc1f",
     "url": "/movies-rdb/index.html"
   },
   {
-    "revision": "e7d82f9d6aee8c72bdfc",
-    "url": "/movies-rdb/static/css/main.1fb5eed1.chunk.css"
+    "revision": "3e018f05c418c9fe36bd",
+    "url": "/movies-rdb/static/css/main.d6e28b67.chunk.css"
   },
   {
     "revision": "24aefab74eba5140186d",
     "url": "/movies-rdb/static/js/2.feff2110.chunk.js"
   },
   {
-    "revision": "e7d82f9d6aee8c72bdfc",
-    "url": "/movies-rdb/static/js/main.cb3aa1fd.chunk.js"
+    "revision": "3e018f05c418c9fe36bd",
+    "url": "/movies-rdb/static/js/main.c3e3f154.chunk.js"
   },
   {
     "revision": "c208811bf6e4add1ac61",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movies-rdb/precache-manifest.084eddddaf76b36e07d0aac6686db91a.js"
+  "/movies-rdb/precache-manifest.093c9867eb204270f611923b2024d0fe.js"
 );
 
 self.addEventListener('message', (event) => {
